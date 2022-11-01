c02e50562507733a811e67feebaf735754fab497
Tyler Myers <tgmyers1@gmail.com>
2022-10-31 20:07:19 -0700

    made it different

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 // const button = document.querySelector('button');
 /* This is a Comment
 It is a multi-line comment */
-console.log('Tyler');
+// console.log('Tyler');
 console.log('Scrambled Eggs')
+
+let name = 'Tyler';
+let favoriteFood = 'Scrambled eggs';
+console.log(name);
+console.log(favoriteFood);
