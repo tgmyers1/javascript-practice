b15d63e9b99269054c5c19a82320011d6bfdb1e4
Tyler Myers <tgmyers1@gmail.com>
2022-10-31 20:19:59 -0700

    made it different again

--- js/script.js
+++ js/script.js
@@ -4,6 +4,16 @@ It is a multi-line comment */
 // console.log('Tyler');
 
 let name = 'Tyler';
+let age = 26;
 let favoriteFood = 'Scrambled eggs';
+
+const interestRate = 2.5
+
 console.log(name);
+console.log(age);
 console.log(favoriteFood);
+console.log(interestRate);
+
+age++;
+
+console.log(age);
