d4152d26f6caedea061a1db25a6d601cf9135a68
Tyler Myers <tgmyers1@gmail.com>
2022-10-31 20:42:49 -0700

    changed variable name

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@
 It is a multi-line comment */
 // console.log('Tyler');
 
-let name = 'Tyler';
+let myName = 'Tyler';
 let age = 26;
 let favoriteFood = 'Scrambled eggs';
 
 const interestRate = 2.5
 
-console.log(name);
+console.log(myName);
 console.log(age);
 console.log(favoriteFood);
 console.log(interestRate);
