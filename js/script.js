a13ed3161cc13a7034a12139936b6395ad8ae617
Tyler Myers <tgmyers1@gmail.com>
2022-11-02 19:10:26 -0700

    completed daily challenge

--- js/script.js
+++ js/script.js
@@ -21,3 +21,13 @@ console.log(age);
 let testStr = 'Supercalifragilisticexpialidocious';
 
 console.log('String is', testStr.length, 'characters long');
+
+let x = 55;
+
+console.log(x);
+console.log(typeof x);
+
+let bigX = 55n;
+
+console.log(bigX);
+console.log(typeof bigX);
